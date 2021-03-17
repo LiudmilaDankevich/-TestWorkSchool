01feaf5134a335fa4d869f7d545da2dcfe9caa35
Liudmial_Dankevich251015 <79874283+LiudmilaDankevich@users.noreply.github.com>
2021-03-18 00:06:58 +0300

    bd

--- pages/db_base.py
+++ pages/db_base.py
@@ -1,13 +1,19 @@
 
 import mysql.connector as mysql
 
-
 db = mysql.connect(host="127.0.0.1",
                     user="root",
                     passwd="",
                     database='litecart')
 cursor = db.cursor()
 
-query = 'SELECT name, sku, quantity, price FROM lc_orders_items WHERE id = 19 '
-cursor.execute(query)
-print(cursor.fetchall())
+
+class DataBase():
+
+    def should_be_order_in_db(self):
+        query = 'SELECT name, sku, quantity, price FROM lc_orders_items WHERE id = 19 '
+        cursor.execute(query)
+        print(cursor.fetchall())
+    # assert check_quantity_duck_text == check_quantity, f'Text on UI {check_quantity_duck_text} is not eq {check_quantity}'
+
+
