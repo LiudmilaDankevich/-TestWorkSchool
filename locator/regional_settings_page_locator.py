5dcdb74b3aaff8a751ef1a55e88d4288ddae8d5a
Liudmial_Dankevich251015 <79874283+LiudmilaDankevich@users.noreply.github.com>
2021-05-13 20:57:02 +0300

    correct locator

--- locator/regional_settings_page_locator.py
+++ locator/regional_settings_page_locator.py
@@ -4,6 +4,6 @@ from selenium.webdriver.common.by import By
 
 class RegionalSettingsPageLocator:
 
-    LOCATOR_REGIONAL_SETTINGS_TEXT = (By.XPATH, '//div[@id="box-regional-settings"]/h1[@class="title"]')
+    LOCATOR_REGIONAL_SETTINGS_TEXT = (By.LINK_TEXT, 'Regional Settings')
     LOCATOR_CURRENCY_SELECT_BUTTON = (By.XPATH, '//select[@name="currency_code"]//option[@value="USD"]')
     LOCATOR_COUNTRY_SELECT_BUTTON  = (By.XPATH, '//select[@name="country_code"]//option[@value="BY"]')
